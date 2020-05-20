46c34daf823079cda930eb0e48139fd77fb4135f
nmoore-demo <64564769+nmoore-demo@users.noreply.github.com>
2020-05-20 19:03:00 +1000

    cosmos update

--- backend/server.js
+++ backend/server.js
@@ -1,77 +1,100 @@
-const appInsights = require('applicationinsights');
-appInsights.setup("4899cc1c-37cc-4765-af41-1ae2e439525e")
-    .setSendLiveMetrics(true)
-    .start();
+const appInsights = require("applicationinsights");
+appInsights
+  .setup("4899cc1c-37cc-4765-af41-1ae2e439525e")
+  .setSendLiveMetrics(true)
+  .start();
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const todoRoutes = express.Router();
 const PORT = 4000;
 
-let Todo = require('./todo.model');
+let Todo = require("./todo.model");
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+const COSMODDB_USER = "";
+const COSMOSDB_PASSWORD = "";
+const COSMOSDB_DBNAME = "Todo";
+const COSMOSDB_HOST = "";
+const COSMOSDB_PORT = 10255;
+
+mongoose.connect(
+  "mongodb://" +
+    COSMOSDB_HOST +
+    ":" +
+    COSMOSDB_PORT +
+    "/" +
+    COSMOSDB_DBNAME +
+    "?ssl=true&replicaSet=globaldb",
+  {
+    auth: {
+      user: COSMODDB_USER,
+      password: COSMOSDB_PASSWORD,
+    },
+  }
+);
+
 const connection = mongoose.connection;
 
-connection.once('open', function() {
-    console.log("MongoDB database connection established successfully");
-})
+connection.once("open", function () {
+  console.log("MongoDB database connection established successfully");
+});
 
-todoRoutes.route('/').get(function(req, res) {
-    Todo.find(function(err, todos) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.json(todos);
-        }
+todoRoutes.route("/").get(function (req, res) {
+  Todo.find(function (err, todos) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+
+todoRoutes.route("/:id").get(function (req, res) {
+  let id = req.params.id;
+  Todo.findById(id, function (err, todo) {
+    res.json(todo);
+  });
+});
+
+todoRoutes.route("/add").post(function (req, res) {
+  let todo = new Todo(req.body);
+  todo
+    .save()
+    .then((todo) => {
+      res.status(200).json({ todo: "todo added successfully" });
+    })
+    .catch((err) => {
+      res.status(400).send("adding new todo failed");
     });
 });
 
-todoRoutes.route('/:id').get(function(req, res) {
-    let id = req.params.id;
-    Todo.findById(id, function(err, todo) {
-        res.json(todo);
-    });
+todoRoutes.route("/update/:id").post(function (req, res) {
+  Todo.findById(req.params.id, function (err, todo) {
+    if (!todo) res.status(404).send("data is not found");
+    else todo.todo_description = req.body.todo_description;
+    todo.todo_responsible = req.body.todo_responsible;
+    todo.todo_priority = req.body.todo_priority;
+    todo.todo_completed = req.body.todo_completed;
+
+    todo
+      .save()
+      .then((todo) => {
+        res.json("Todo updated");
+      })
+      .catch((err) => {
+        res.status(400).send("Update not possible");
+      });
+  });
 });
 
-todoRoutes.route('/add').post(function(req, res) {
-    let todo = new Todo(req.body);
-    todo.save()
-        .then(todo => {
-            res.status(200).json({'todo': 'todo added successfully'});
-        })
-        .catch(err => {
-            res.status(400).send('adding new todo failed');
-        });
-});
+app.use("/todos", todoRoutes);
 
-todoRoutes.route('/update/:id').post(function(req, res) {
-    Todo.findById(req.params.id, function(err, todo) {
-        if (!todo)
-            res.status(404).send('data is not found');
-        else
-            todo.todo_description = req.body.todo_description;
-            todo.todo_responsible = req.body.todo_responsible;
-            todo.todo_priority = req.body.todo_priority;
-            todo.todo_completed = req.body.todo_completed;
-
-            todo.save().then(todo => {
-                res.json('Todo updated');
-            })
-            .catch(err => {
-                res.status(400).send("Update not possible");
-            });
-    });
-});
-
-app.use('/todos', todoRoutes);
-
-app.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
+app.listen(PORT, function () {
+  console.log("Server is running on Port: " + PORT);
 });
